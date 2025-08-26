acecab898320ceb2b74720bacf86cfe0a972cca2
ThunderTejas <tejas.naanu2004@gmail.com>
2025-08-26 14:11:21 +0530

    logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,25 @@ import React from 'react';
 // REMOVE THIS LINE: import styles from './Header.module.css'; 
 
 const Header = () => {
-  return (
-    // CHANGE classNames from {styles.x} to "x"
-    <header className="header">
-      <div className="logo">CHEXSHIRT</div>
-      <nav className="navLinks">
-        <a href="#">SHIRTS</a>
-        <a href="#">SHOP</a>
-        <a href="#">OUR STORY</a>
-        <a href="#">FAVORITES</a>
-      </nav>
-      <div className="userActions">
-        <span>🔍</span>
-        <span>🛒</span>
-        <span>👤</span>
-      </div>
-    </header>
-  );
+    return (
+        // CHANGE classNames from {styles.x} to "x"
+        <header className="header">
+            <div className="logo">
+                <img src="/logo.png" alt="CHEXSHIRT Logo" className="logo-img" />
+            </div>
+            <nav className="navLinks">
+                <a href="#">SHIRTS</a>
+                <a href="#">SHOP</a>
+                <a href="#">OUR STORY</a>
+                <a href="#">FAVORITES</a>
+            </nav>
+            <div className="userActions">
+                <span>🔍</span>
+                <span>🛒</span>
+                <span>👤</span>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
